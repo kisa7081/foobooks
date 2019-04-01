3f8fca9556c623064d4460053aac3d702b01f937
Daniel McCullough <kisa7081@gmail.com>
2019-03-31 21:12:27 -0500

    Changes after week 8 videos.

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 Route::view('/', 'welcome');
 
+Route::get('/books/search', 'BookController@search');
+Route::get('/books/search-process', 'BookController@searchProcess');
+
 Route::get('/books', 'BookController@index');
+
+
+Route::get('/books/create', 'BookController@create');
+Route::post('/books', 'BookController@store');
 
 Route::get('/books/{title}', 'BookController@show');
 
 
 Route::any('/practice/{n?}', 'PracticeController@index');
+
