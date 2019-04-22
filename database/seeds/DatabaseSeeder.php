855e85a3cc3ece1f35e53bbef00393dcaad3c6f3
Daniel McCullough <kisa7081@gmail.com>
2019-04-21 21:29:26 -0500

    Haven't done a commit in a while.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,9 +8,13 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      *
      * @return void
+     *
+     * php artisan db:seed
+     * or
+     * php artisan migrate:fresh --seed
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(BooksTableSeeder::class);
     }
 }
