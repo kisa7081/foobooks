a651d1f74664588c0ee8132594a93d2e7ee996bb
Daniel McCullough <kisa7081@gmail.com>
2019-04-28 14:13:26 -0500

    Added a seeder (last week actually).

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      * php artisan db:seed
      * or
      * php artisan migrate:fresh --seed
+     *
+     * c6650ab736e2948cf52fd7106582310e39ad9e3419332690
      */
     public function run()
     {
